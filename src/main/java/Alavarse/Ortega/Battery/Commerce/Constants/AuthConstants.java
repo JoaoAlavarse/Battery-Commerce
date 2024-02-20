b6b74b7150467ffaf14d31ae8b09fed0ab44cbf2
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-02-19 21:00:50 -0300

    Feat: Auth Constants

---
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Constants/AuthConstants.java
@@ -0,0 +1,6 @@
+package Alavarse.Ortega.Battery.Commerce.Constants;
+
+public class AuthConstants {
+    public static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_+=<>?0123456789";
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+}
