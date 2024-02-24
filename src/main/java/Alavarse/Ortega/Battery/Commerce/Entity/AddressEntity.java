75ddc3a2d716ec35966d625c78e568779cd554c4
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-02-24 16:25:26 -0300

    Feat: Relationship with UserEntity

--- src/main/java/Alavarse/Ortega/Battery/Commerce/Entity/AddressEntity.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Entity/AddressEntity.java
@@ -1,5 +1,7 @@
 package Alavarse.Ortega.Battery.Commerce.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,11 +12,11 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"user"})
 public class AddressEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String addressId;
-    private String userId;
     private String address;
     private String number;
     private String complement;
@@ -22,13 +24,17 @@ public class AddressEntity {
     private String state;
     private String CEP;
 
-    public AddressEntity(String userId, String address, String number, String complement, String city, String state, String CEP) {
-        this.userId = userId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    public AddressEntity(String address, String number, String complement, String city, String state, String CEP, UserEntity user) {
         this.address = address;
         this.number = number;
         this.complement = complement;
         this.city = city;
         this.state = state;
         this.CEP = CEP;
+        this.user = user;
     }
 }
