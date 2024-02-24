e22f704998cf6ba65b7573b45b1526a53300bea8
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-02-24 16:26:06 -0300

    Feat: AddressService

---
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Service/AddressService.java
@@ -0,0 +1,37 @@
+package Alavarse.Ortega.Battery.Commerce.Service;
+
+import Alavarse.Ortega.Battery.Commerce.DTO.AddressDTO;
+import Alavarse.Ortega.Battery.Commerce.Entity.AddressEntity;
+import Alavarse.Ortega.Battery.Commerce.Entity.UserEntity;
+import Alavarse.Ortega.Battery.Commerce.Exceptions.UserExceptions.ErrorWhileGettingUsersException;
+import Alavarse.Ortega.Battery.Commerce.Repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressService {
+    @Autowired
+    private AddressRepository repository;
+    @Autowired
+    private UserService userService;
+
+
+    public AddressEntity create(AddressDTO data){
+        Optional<UserEntity> optionalUser = userService.findById(data.userId());
+        if (optionalUser.isEmpty()){
+            throw new ErrorWhileGettingUsersException();
+        }
+        UserEntity newUser = optionalUser.get();
+        AddressEntity address = new AddressEntity(data.address(), data.number(), data.complement(), data.city(), data.state(), data.CEP(), newUser);
+        repository.save(address);
+        return address;
+    }
+
+    public List<AddressEntity> getAll(){
+        return repository.findAll();
+    }
+}
