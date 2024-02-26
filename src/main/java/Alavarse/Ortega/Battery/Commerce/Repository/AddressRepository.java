7cd71804147dc984e0673a2f4403f03e29f2324b
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-02-26 00:13:22 -0300

    Fix: findByUser

--- src/main/java/Alavarse/Ortega/Battery/Commerce/Repository/AddressRepository.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package Alavarse.Ortega.Battery.Commerce.Repository;
 
 import Alavarse.Ortega.Battery.Commerce.Entity.AddressEntity;
+import Alavarse.Ortega.Battery.Commerce.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity, String> {
-    List<AddressEntity> findByUserId(String userId);
+    List<AddressEntity> findByUser(UserEntity userId);
 }
