7775d11a6b401bd869a853df1be492f85496dbb9
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-03-04 22:07:06 -0300

    Feat: Cart Repository

---
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Repository/CartRepository.java
@@ -0,0 +1,12 @@
+package Alavarse.Ortega.Battery.Commerce.Repository;
+
+import Alavarse.Ortega.Battery.Commerce.Entity.CartEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<CartEntity, String> {
+    @Query(value = "SELECT * FROM promotion p WHERE p.start_date < CURRENT_DATE - INTERVAL '2 days'", nativeQuery = true)
+    List<CartEntity> findByTwoDaysOfExistence();
+}
