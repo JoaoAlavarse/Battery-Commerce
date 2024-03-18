1dbd3bb3e39feb4a8adf1c33979ea9c123a0d61b
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-03-18 16:29:36 -0300

    Feat: CartController

---
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Controller/CartController.java
@@ -0,0 +1,9 @@
+package Alavarse.Ortega.Battery.Commerce.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+}
