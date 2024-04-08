3b8122c5d16a3554499f9c76a62d6be8e080c777
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-04-07 22:59:50 -0300

    Feat: CORS config

---
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Infrastructure/WebConfig/WebConfig.java
@@ -0,0 +1,20 @@
+package Alavarse.Ortega.Battery.Commerce.Infrastructure.WebConfig;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ServletComponentScan
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
