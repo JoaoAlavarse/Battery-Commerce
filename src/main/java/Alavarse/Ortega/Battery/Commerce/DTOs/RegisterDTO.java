19bcc63810f24eec1b112cf7e81d69ceca8dd02f
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-04-09 01:27:07 -0300

    Fix: NotNull and NotBlank validation

--- src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/RegisterDTO.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/RegisterDTO.java
@@ -1,6 +1,10 @@
 package Alavarse.Ortega.Battery.Commerce.DTOs;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-public record RegisterDTO(@NotBlank String email, @NotBlank String password, @NotBlank String name, @NotBlank String document) {
+
+public record RegisterDTO(@NotBlank String email, @NotBlank String password, @NotBlank String name, @NotBlank String document){
 }
