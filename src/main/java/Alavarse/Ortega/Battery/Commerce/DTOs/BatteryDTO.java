c5a8de568dddc34f3e96248eb150a375fa0fe55e
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-04-22 02:40:16 -0300

    Fix: Bean validations

--- src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/BatteryDTO.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/BatteryDTO.java
@@ -2,14 +2,15 @@ package Alavarse.Ortega.Battery.Commerce.DTOs;
 
 import Alavarse.Ortega.Battery.Commerce.Enums.BatteryStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record BatteryDTO(
         @NotBlank String name,
         @NotBlank String description,
-        @NotBlank BigDecimal value,
-        @NotBlank Integer quantity,
+        @NotNull  BigDecimal value,
+        @NotNull Integer quantity,
         BatteryStatus status
 ) {
 }
--- src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/PromotionDTO.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/PromotionDTO.java
@@ -2,11 +2,12 @@ package Alavarse.Ortega.Battery.Commerce.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record PromotionDTO(
-        @NotBlank @JsonFormat(pattern = "dd/MM/yyyy") LocalDate expirationDate,
-        @NotBlank Integer percentage,
+        @NotNull @JsonFormat(pattern = "dd/MM/yyyy") LocalDate expirationDate,
+        @NotNull Integer percentage,
         @NotBlank String code) {
 }
