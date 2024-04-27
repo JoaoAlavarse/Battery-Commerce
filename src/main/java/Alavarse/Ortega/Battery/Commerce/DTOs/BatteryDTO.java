411cd1c2902bd80f35c1ce91add9ed1fd38622d4
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-04-26 21:06:58 -0300

    Fix: Min value for numbers

--- src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/BatteryDTO.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/BatteryDTO.java
@@ -1,6 +1,7 @@
 package Alavarse.Ortega.Battery.Commerce.DTOs;
 
 import Alavarse.Ortega.Battery.Commerce.Enums.BatteryStatus;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,7 +10,7 @@ import java.math.BigDecimal;
 public record BatteryDTO(
         @NotBlank String name,
         @NotBlank String description,
-        @NotNull  BigDecimal value,
+        @NotNull @Min(0) BigDecimal value,
         @NotNull Integer quantity,
         BatteryStatus status
 ) {
--- src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/DiscountDTO.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/DTOs/DiscountDTO.java
@@ -1,11 +1,12 @@
 package Alavarse.Ortega.Battery.Commerce.DTOs;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.math.BigDecimal;
 
 public record DiscountDTO(
         @NotBlank String userId,
-        @NotBlank BigDecimal totalValue
+        @NotBlank @Min(0) BigDecimal totalValue
 ) {
 }
