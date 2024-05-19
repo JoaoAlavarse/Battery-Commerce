71030e5ac695bfebad1e8e02018dc7c765e06a65
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-05-19 03:43:18 -0300

    Fix: Route

--- src/main/java/Alavarse/Ortega/Battery/Commerce/Controllers/AddressController.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Controllers/AddressController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/address")
+@RequestMapping("address")
 public class AddressController {
     @Autowired
     private AddressService service;
