3900a95bde4b94881883d3e852c14afe04c516f1
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-06-01 00:34:13 -0300

    Fix: Card number verify regex

--- src/main/java/Alavarse/Ortega/Battery/Commerce/Services/CardService.java
+++ src/main/java/Alavarse/Ortega/Battery/Commerce/Services/CardService.java
@@ -136,18 +136,27 @@ public class CardService {
     }
 
     public String getCardType(String cardNumber) {
-        if (cardNumber.startsWith("4")) {
+        if (cardNumber.matches("^4[0-9]{12}(?:[0-9]{3})?$")) {
             return "Visa";
-        } else if (cardNumber.matches("^5[1-5].*")) {
+        } else if (cardNumber.matches("^5[1-5][0-9]{14}$")) {
             return "MasterCard";
-        } else if (cardNumber.matches("^3[47].*")) {
+        } else if (cardNumber.matches("^3[47][0-9]{13}$")) {
             return "American Express";
-        } else if (cardNumber.matches("^6(?:011|5|4[4-9]|22).*")) {
+        } else if (cardNumber.matches("^6(?:011|5[0-9]{2})[0-9]{12}$")) {
             return "Discover";
-        } else if (cardNumber.matches("^35(?:2[89]|[3-8][0-9]).*")) {
+        } else if (cardNumber.matches("^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]" +
+                "|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|" +
+                "[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]" +
+                "|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))")) {
+            return "Elo";
+        } else if (cardNumber.matches("^(?:2131|1800|35\\d{3})\\d{11}")) {
             return "JCB";
         } else if (cardNumber.matches("^3(?:0[0-5]|[68]).*")) {
             return "Diners Club";
+        } else if  (cardNumber.matches("^((?!504175))^((?!5067))(^50[0-9])")){
+            return "Aura";
+        } else if  (cardNumber.matches("^606282|^3841(?:[0|4|6]{1})0/")){
+            return "HiperCard";
         } else {
             throw new UnknownCardFlagException();
         }
